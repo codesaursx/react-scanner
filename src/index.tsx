13f64a6fc5d5c6655519373e4b4aed796e9f992c
Angel Martinez <angel.martineztrc@gmail.com>
2023-03-31 01:07:25 -0500

    chore:  create component

---
+++ src/index.tsx
@@ -0,0 +1,59 @@
+import React, { useCallback, useEffect, useRef } from 'react';
+import { BrowserMultiFormatReader, Result } from '@zxing/library';
+import Webcam from 'react-webcam';
+
+type ScannerProps = {
+  onUpdate: (error: unknown, data?: Result) => void;
+  onError?: (error: string | DOMException) => void;
+  width?: number | string;
+  height?: number | string;
+  facingMode?: 'environment' | 'user';
+  torch?: boolean;
+  delay?: number;
+  videoConstraints?: MediaTrackConstraints;
+  stopStream?: boolean;
+};
+
+export const Scanner = ({
+  onUpdate,
+  onError,
+  width = '100%',
+  height = '100%',
+  facingMode = 'environment',
+  delay = 500,
+  videoConstraints,
+}: ScannerProps) => {
+  const webcamRef = useRef(null);
+
+  const handleCapture = useCallback(() => {
+    const reader = new BrowserMultiFormatReader();
+    // @ts-ignore
+    const imageSrc = webcamRef?.current?.getScreenshot();
+    if (imageSrc) {
+      reader
+        .decodeFromImage(undefined, imageSrc)
+        .then(res => onUpdate(null, res))
+        .catch(e => onUpdate(e));
+    }
+  }, [onUpdate]);
+
+  useEffect(() => {
+    const interval = setInterval(handleCapture, delay);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  return (
+    <Webcam
+      width={width}
+      height={height}
+      ref={webcamRef}
+      screenshotFormat="image/jpeg"
+      videoConstraints={videoConstraints ?? { facingMode }}
+      audio={false}
+      onUserMediaError={onError}
+    />
+  );
+};
