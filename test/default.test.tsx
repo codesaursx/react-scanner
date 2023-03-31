c51f9320b39f83a8a853e0e46d8e49699d27d314
Angel Martinez <angel.martineztrc@gmail.com>
2023-03-31 01:08:43 -0500

    feat:  add test case

--- test/default.test.tsx
+++ test/default.test.tsx
@@ -1,11 +1,14 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Thing } from '../src';
+import { Scanner } from '../src';
 
 describe('it', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<Thing />, div);
+    ReactDOM.render(
+      <Scanner onUpdate={(e, data) => console.log(e, data)} />,
+      div
+    );
     ReactDOM.unmountComponentAtNode(div);
   });
 });
